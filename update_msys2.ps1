d22eb70d640340df8468b3d45e4e3834de0c04f1
MSP-Greg <Greg.mpls@gmail.com>
2022-01-25 14:14:08 -0600

    Update update_msys2.ps1

--- update_msys2.ps1
+++ update_msys2.ps1
@@ -1,8 +1,6 @@
 $dash = "-" * 40
 
-pacman.exe -R --noconfirm --needed --noprogressbar autoconf2.71
-pacman.exe -R --noconfirm --needed --noprogressbar autoconf2.69
-pacman.exe -R --noconfirm --needed --noprogressbar autoconf2.13
+pacman.exe -Rdd --noconfirm --noprogressbar autoconf
 
 Write-Host "`n$dash pacman --noconfirm -Syyuu"
 pacman.exe -Syyuu --noconfirm
@@ -15,7 +13,9 @@ taskkill /f /fi "MODULES eq msys-2.0.dll"
 $pkgs = 'autoconf-wrapper autogen automake-wrapper bison diffutils libtool m4 make patch texinfo texinfo-tex compression'
 
 Write-Host "`n$dash Install MSYS2 $pkgs"
-pacman.exe -S --noconfirm --needed --noprogressbar $pkgs.split(' ')
+pacman.exe -S --noconfirm --needed --overwrite --noprogressbar $pkgs.split(' ')
 
 Write-Host "`n$dash Clean packages"
 pacman.exe -Scc --noconfirm
+
+pacman -Q
